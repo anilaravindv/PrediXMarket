3b1d6688ab3c448f648304fec44f2ae8796df9e5
Anil Aravind <v.anilaravind@gmail.com>
2024-02-04 17:26:16 +0530

    nav bar fix

--- app/src/components/navigation/index.tsx
+++ app/src/components/navigation/index.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useAnchorWallet, useWallet } from "@solana/wallet-adapter-react";
 import { WalletDisconnectButton, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import Logo from 'assets/logo1.png'
+import Logo from 'assets/logo.png'
 import Background from 'assets/Hero.png'
 import { observer } from "mobx-react-lite";
 import { useStores } from "context/StoreComponent";
@@ -45,143 +45,99 @@ const CreateMarketLink = observer((Props: ICreateMarketLink) => {
 const Navigation = () => {
     const { connected } = useWallet();
     const [isNavOpen, setIsNavOpen] = useState(false);
-
+    const [toggleMenu, setToggleMenu] = useState(false);
     return (
-        // <header classNameName="fixed w-screen space-x-14 z-10" style={{
-        //     backgroundImage: `url(${Background})`,
-        //     backgroundPosition: 'center',
-        //     backgroundSize: 'cover', backgroundRepeat: 'no-repeat',
-        //     backgroundAttachment: 'fixed'
-        // }}>
-        //     <div classNameName="xl:mx-16 xl:px-16 flex justify-between">
-        //         <Link to="/">
-        //             <div classNameName="m-2 p-2 flex justify-start items-center">
-        //                 <img src={Logo} alt="logo" style={{ width: "40%" }} />
-        //                 {/* <div classNameName="font-semibold">Prediction Market</div> */}
-        //             </div>
-        //         </Link>
-        //         <div classNameName="hidden lg:flex justify-between items-center m-2 p-2 ">
-        //             <div classNameName="flex justify-between items-center space-x-9 font-semibold">
-        //                 {
-        //                     connected ? (
-        //                         <>
-        //                             <CreateMarketLink />
-        //                             <Link to="/markets">Markets</Link>
-        //                             {<>
-        //                                 <Link to="/portfolio">Portfolio</Link>
-        //                                 <Link to="/leaderboard">Leaderboard</Link>
-        //                             </>}
-        //                             <span>Portfolio</span>
-        //                             <span>Leaderboard</span>
-        //                         </>
-        //                     ) : <Link to="/markets">Markets</Link>
-        //                 }
-        //                 {
-        //                     connected ? <WalletDisconnectButton /> : <WalletMultiButton />
-        //                 }
-        //             </div>
-        //         </div>
-        //         <section classNameName="MOBILE-MENU flex items-center lg:hidden">
-        //             <GiHamburgerMenu classNameName="w-10 h-10 p-2 pr-3"
-        //                 onClick={() => setIsNavOpen((prev) => !prev)} />
-        //             <div classNameName={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
-        //                 <div classNameName="flex justify-between items-center absolute top-0 right-1 left-0">
-        //                     <Link to="/" onClick={() => setIsNavOpen(false)}>
-        //                         <div classNameName="m-2 p-2 flex justify-start items-center">
-        //                             <img src={Logo} alt="logo" />
-        //                             <div classNameName="font-semibold">Prediction Market</div>
-        //                         </div>
-        //                     </Link>
-        //                     <AiOutlineCloseCircle classNameName="w-10 h-10 p-2"
-        //                         onClick={() => setIsNavOpen(false)} />
+<div className="app">
+      <nav>
+        <div className="max-b-7xl mx-auto bg-orange-70">
+          <div className="flex mx-auto justify-between w-5/6 ">
+            {/* Primary menu and logo */}
+            <div className="flex items-center gap-16 my-12">
+              {/* logo */}
+              <div>
+                <a
+                  href="/"
+                  className="flex gap-1 font-bold text-gray-700 items-center "
+                >
+                  <img src={Logo} alt="logo" className="h-10 w-10 text-primary" />
 
-        //                 </div>
-        //                 <div classNameName="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px] font-semibold">
-        //                     {
-        //                         connected ? (
-        //                             <>
-        //                                 <CreateMarketLink onClick={() => setIsNavOpen(false)} />
-        //                                 <Link to="/markets" onClick={() => setIsNavOpen(false)}>Markets</Link>
-        //                                 {/* {<>
-        //                                 <Link to="/portfolio" onClick={() => setIsNavOpen(false)}>Portfolio</Link>
-        //                                 <Link to="/leaderboard" onClick={() => setIsNavOpen(false)}>Leaderboard</Link>
-        //                                 </>} */}
-        //                                 <span>Portfolio</span>
-        //                                 <span>Leaderboard</span>
-        //                             </>
-        //                         ) : <Link to="/markets">Markets</Link>
-        //                     }
-        //                     {
-        //                         connected
-        //                             ? <WalletDisconnectButton onClick={() => setIsNavOpen(false)} />
-        //                             : <WalletMultiButton onClick={() => setIsNavOpen(false)} />
-        //                     }
-        //                 </div>
-        //             </div>
-        //         </section>
-        //     </div>
-        //     <style>{`
-        //         .hideMenuNav {
-        //              display: none;
-        //         }
-        //         .showMenuNav {
-        //             display: block;
-        //             position: absolute;
-        //             width: 100%;
-        //             height: 100vh;
-        //             top: 0;
-        //             left: 0;
-        //             background: url(${Background});
-        //             z-index: 10;
-        //             display: flex;
-        //             flex-direction: column;
-        //             justify-content: center;
-        //             align-items: center;
-        //         }
-        //     `}</style>
-        // </header>
-
-        <header>
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <nav className="navbar navbar-expand-lg">
-                <div className="container-fluid">
-                  <a className="navbar-brand" href="home.html"><img src="assets/images/logo.png" alt="" /></a>
-                  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon">
-                      <i className="fa fa-bars" aria-hidden="true"></i>
-                    </span>
-                  </button>
-                  <div className="collapse navbar-collapse" id="navbarScroll">
-                    {
-                      connected ? (
-                        <>
-                          <ul className="navbar-nav m-auto my-2 my-lg-0">
-                            <CreateMarketLink />
-                            <li className="nav-item">
-                              <Link className="nav-link" to="/markets">Markets</Link>
-                            </li>
-                            <li className="nav-item">
-                              <Link className="nav-link" to="/portfolio">Portfolio</Link>
-                            </li>
-                            <li className="nav-item">
-                              <Link className="nav-link" to="/leaderboard">Leaderboard</Link>
-                            </li>
-                          </ul>
-                        </>
-                      ) : <Link to="/markets">Markets</Link>
-                    }
-                    <div className="d-flex">
-                      <button className="btn btn-connect" type="submit">Disconnect <i className="fa fa-angle-right" aria-hidden="true"></i></button>
-                    </div>
-                  </div>
+                  <span>PredixMarket</span>
+                </a>
+              </div>
+              {/* primary */}
+              <div className="hidden lg:flex gap-12 ">
+                  {
+                    connected ? (
+                      <>
+                        <CreateMarketLink />
+                        <Link to="/markets">Markets</Link>
+                        {/* {<>
+                                        <Link to="/portfolio">Portfolio</Link>
+                                        <Link to="/leaderboard">Leaderboard</Link>
+                                    </>} */}
+                        <span>Portfolio</span>
+                        <span>Leaderboard</span>
+                      </>
+                    ) : <Link to="/markets">Markets</Link>
+                  }
+                  {
+                    connected ? <WalletDisconnectButton /> : <WalletMultiButton />
+                  }
+              </div>
+            </div>
+            {/* secondary */}
+            <div className="flex gap-6">
+              <div className="hidden xs:flex items-center gap-10">
+                <div className="hidden lg:flex items-center gap-2">
+                  {/* <MoonIcon className="h-6 w-6" /> */}
+                  {/* <SunIcon className="h-6 w-6" /> */}
                 </div>
-              </nav>
+                <div>
+                  <button className="rounded-full border-solid border-2 border-gray-300 py-2 px-4 hover:bg-gray-700 hover:text-gray-100">
+                    Free Trial
+                  </button>
+                </div>
+              </div>
+              {/* Mobile navigation toggle */}
+              <div className="lg:hidden flex items-center">
+                <button onClick={() => setToggleMenu(!toggleMenu)}>
+                  <button data-collapse-toggle="navbar-dropdown" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
+                    <span className="sr-only">Open main menu</span>
+                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                    </svg>
+                  </button>
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </header>
+        {/* mobile navigation */}
+        <div
+          className={`fixed z-40 w-full  bg-gray-100 overflow-hidden flex flex-col lg:hidden gap-12  origin-top duration-700 ${
+            !toggleMenu ? "h-0" : "h-full"
+          }`}
+        >
+          <div className="px-8">
+            <div className="flex flex-col gap-8 tracking-wider">
+              {
+                    connected ? (
+                      <>
+                        <CreateMarketLink />
+                        <Link to="/markets">Markets</Link>
+                        <span>Portfolio</span>
+                        <span>Leaderboard</span>
+                      </>
+                    ) : <Link to="/markets">Markets</Link>
+                  }
+                  {
+                    connected ? <WalletDisconnectButton /> : <WalletMultiButton />
+                  }
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
     );
 }
 export default Navigation;
